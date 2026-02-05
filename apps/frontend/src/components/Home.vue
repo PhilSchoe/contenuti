46177d741ce448b021d6dbc8034b71a0704e9988
PhilSchoe <p.schoemacker@gmx.de>
2026-02-05 18:35:46 +0100

    Add shadcn vue

---
+++ apps/frontend/src/components/Home.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { Button } from "./ui/button";
+</script>
+
+<template>
+  <h1>Welcome to the Home Page!</h1>
+  <p>This is the main landing page of the application for now.</p>
+  <Button variant="default" size="default"> Click Me </Button>
+</template>
+
+<style scoped></style>
