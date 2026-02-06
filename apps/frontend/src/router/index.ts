127714140907d768e69b6a439fde79f594bb50a6
PhilSchoe <p.schoemacker@gmx.de>
2026-02-06 22:28:35 +0100

    Move and rename Home.vue

--- apps/frontend/src/router/index.ts
+++ apps/frontend/src/router/index.ts
@@ -6,7 +6,7 @@ const router = createRouter({
     {
       path: "/",
       name: "Home",
-      component: () => import("../components/Home.vue"),
+      component: () => import("../views/HomeView.vue"),
     },
     {
       path: "/signin",
